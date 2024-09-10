c10ea67d11c076329e38bd88da39521fb2fdbbe3
nandu03d <nandu03d@gmail.com>
2024-09-10 15:58:28 +0530

    instance type and tags

--- variables.tf
+++ variables.tf
@@ -1,9 +1,19 @@
 variable "ami_id" {
     type = string
-    default = "ami-090252cbe067a9e58"
+    default = "ami-041e2ea9402c46c32"
 }
 
 variable "security_group_ids" {
     type = list
-    default = ["sg-0fea5e49e962e81c9"] #replace with your SG ID.
+    default = ["sg-079211a4577b7ad6c"]
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+    type = string
+}
+
+variable "tags" {
+    type = map
+    default = {} # this means empty, so not mandatory
 }
