a75f5110a27d8f0e25041e77c05a10e69569927a
nandu03d <nandu03d@gmail.com>
2024-09-10 15:39:28 +0530

    add security group

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "db" {
 
     ami = var.ami_id
+    vpc_security_group_ids = var.security_group_ids
 }
--- variables.tf
+++ variables.tf
@@ -3,3 +3,7 @@ variable "ami_id" {
     default = "ami-090252cbe067a9e58"
 }
 
+variable "security_group_ids" {
+    type = list
+    default = ["sg-0fea5e49e962e81c9"] #replace with your SG ID.
+}
